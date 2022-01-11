f2add76f76528856de6b94e079265079126ebca4
ZhangXD <1296753187@qq.com>
2022-01-11 17:28:03 +0800

    初始化日志logging

--- FileValidate.py
+++ FileValidate.py
@@ -1,20 +1,30 @@
 import hashlib
+import logging
 import os.path
-from progress.bar import ShadyBar
 
 
 # 验证文件完整性
 class FileValidate:
 
     def __init__(self):
-        self.bar = ShadyBar()
+        LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+        DATE_FORMAT = "%Y/%m/%d %H:%M:%S"
+        logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+
+    def verify(self, pathA, pathB):
+        sizeA = os.path.getsize(pathA)
+        sizeB = os.path.getsize(pathB)
+        if sizeA != sizeB:
+            logging.warning("文件大小不匹配")
+        else:
+            logging.info("待验证的文件大小为: {0}".format(sizeA))
 
     def validateFile(self, pathA, pathB):
         # 判断路径是否存在
         if not os.path.exists(pathA):
-            print("[{0}]文件或目录不存在".format(pathA))
+            logging.warn("[{0}]文件或目录不存在".format(pathA))
         if not os.path.exists(pathB):
-            print("[{0}]文件或目录不存在".format(pathB))
+            logging.warn("[{0}]文件或目录不存在".format(pathB))
         # 若都是文件，则判断文件的md5码是否相同
         if os.path.isfile(pathA) and os.path.isfile(pathB):
             md5A = hashlib.md5()
@@ -28,9 +38,9 @@ class FileValidate:
                     if not data2:
                         md5B.update(data2)
             if md5A.hexdigest() != md5B.hexdigest():
-                print("文件{0}与文件{1}内容不一致".format(pathA, pathB))
-            # else:
-            #     print("文件{0}与文件{1}校验完成".format(pathA, pathB))
+                logging.warning("文件{0}与文件{1}内容不一致".format(pathA, pathB))
+            else:
+                logging.info("文件{0}与文件{1}校验完成".format(pathA, pathB))
         # 若都是目录，则递归调用该函数
         if os.path.isdir(pathA) and os.path.isdir(pathB):
             pathsA = []
@@ -42,16 +52,16 @@ class FileValidate:
             list.sort(pathsA)
             list.sort(pathsB)
             if len(pathsA) != len(pathsB):
-                print("目录{0}与目录{1}文件数量不一致".format(pathA, pathB))
+                logging.warning("目录{0}与目录{1}文件数量不一致".format(pathA, pathB))
             else:
                 for i in range(len(pathsA)):
                     self.validateFile(pathsA[i], pathsB[i])
         if (os.path.isdir(pathA) and os.path.isfile(pathB)) or (os.path.isdir(pathB) and os.path.isfile(pathA)):
-            print("路径{0}和路径{1}目录文件不匹配".format(pathA, pathB))
+            logging.warning("路径{0}和路径{1}目录文件不匹配".format(pathA, pathB))
 
 
 if __name__ == '__main__':
     filevalidate = FileValidate()
     pathA = r"E:\尚硅谷Java学科全套教程（总207.77GB）"
     pathB = r"F:\尚硅谷Java学科全套教程（总207.77GB）"
-    filevalidate.validateFile(pathA, pathB)
+    logging.info("这是一个error")
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-import time
+from time import sleep
+from tqdm import tqdm
 
-from progress.bar import ShadyBar
-
-bar = ShadyBar()
-for i in bar(range(10)):
-    time.sleep(0.1)
+# 这里同样的，tqdm就是这个进度条最常用的一个方法
+# 里面存一个可迭代对象
+for i in tqdm(range(0, 500), colour="white"):
+    # 模拟你的任务
+    sleep(0.01)
