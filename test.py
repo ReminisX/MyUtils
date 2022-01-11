dfabbfafc06bb2142a4fea661a174e06f0fcc2ea
ZhangXD <1296753197@qq.com>
2022-01-11 23:20:20 +0800

    test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-import time
+import os.path
 
-from progress.bar import ShadyBar
-
-bar = ShadyBar()
-for i in bar(range(10)):
-    time.sleep(0.1)
+folder_path = r"D:\CPU-Z"
+full_size = 0
+for parent, dirs, files in os.walk(folder_path):
+    full_size = sum(os.path.getsize(os.path.join(parent, file)) for file in files)
+print(full_size, "%.2f MB" % (full_size/1024/1024))
